9d93a7b394aa95897fb7977bb0d74b93eafa8eeb
Jose V. Alcala-Burgos <vidal.alcala@gmail.com>
2022-09-07 17:19:12 -0500

    Update targets list

--- pkgs/misc/ghostscript/default.nix
+++ pkgs/misc/ghostscript/default.nix
@@ -90,7 +90,7 @@ stdenv.mkDerivation rec {
 
   # don't build/install statically linked bin/gs
   buildFlags = [ "so" ];
-  installTargets = [ "so" "install" ];
+  installTargets = [ "so" ];
 
   postInstall = ''
     ln -s gsc "$out"/bin/gs
