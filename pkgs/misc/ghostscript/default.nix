a99da21835396c7b4f412c692a75c744d95c848d
Jose V. Alcala-Burgos <vidal.alcala@gmail.com>
2022-09-07 15:28:38 -0500

    Fix bug in targets list

--- pkgs/misc/ghostscript/default.nix
+++ pkgs/misc/ghostscript/default.nix
@@ -90,7 +90,7 @@ stdenv.mkDerivation rec {
 
   # don't build/install statically linked bin/gs
   buildFlags = [ "so" ];
-  installTargets = [ "so", "install" ];
+  installTargets = [ "so" "install" ];
 
   postInstall = ''
     ln -s gsc "$out"/bin/gs
